2c1ecec63917f04dcafa5100c4256fb98af9bd0f
Munjapara Fenny <fennymunjapara0309@gmail.com>
2026-04-24 20:21:18 +0530

    Update PR20_CodingExercise.java

--- PR20_CodingExercise.java
+++ PR20_CodingExercise.java
@@ -9,16 +9,16 @@ public class PR20_CodingExercise {
     public static void main(String[] args) {
 
         try {
-            // Check if arguments are provided
+            
             if (args.length < 2) {
                 throw new ArrayIndexOutOfBoundsException("Please provide numerator and denominator");
             }
 
-            // Convert command-line arguments to integers
+           
             int numerator = Integer.parseInt(args[0]);
             int denominator = Integer.parseInt(args[1]);
 
-            // Perform division
+           
             int result = numerator / denominator;
 
             System.out.println("Numerator   = " + numerator);
@@ -41,24 +41,3 @@ public class PR20_CodingExercise {
     }
 }
 
-/*
-Output:
-
-Case 1:
-java CommandLineDivision 10 2
-Numerator   = 10
-Denominator = 2
-Result      = 5
-
-Case 2:
-java CommandLineDivision 10
-Error: Please provide numerator and denominator
-
-Case 3:
-java CommandLineDivision 10 0
-Error: Cannot divide by zero
-
-Case 4:
-java CommandLineDivision 10 abc
-Error: Invalid number format
-*/
