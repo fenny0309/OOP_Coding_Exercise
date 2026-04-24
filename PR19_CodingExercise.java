d59165a1360f85ff8db77135b3e10f0709ca2f46
Munjapara Fenny <fennymunjapara0309@gmail.com>
2026-04-24 19:48:41 +0530

    Update PR19_CodingExercise.java

--- PR19_CodingExercise.java
+++ PR19_CodingExercise.java
@@ -1,58 +1,47 @@
-package exam;
-
 import student.Student;
 import java.util.Scanner;
 
 public class Result extends Student {
-
     int marks1, marks2, marks3;
 
-    public Result(int rollNo, String name, int m1, int m2, int m3) {
-        super(rollNo, name);
-        this.marks1 = m1;
-        this.marks2 = m2;
-        this.marks3 = m3;
+    public Result(int r, String n, int m1, int m2, int m3) {
+        super(r, n);
+        marks1 = m1;
+        marks2 = m2;
+        marks3 = m3;
     }
 
     public void displayResult() {
+        int total = marks1 + marks2 + marks3;
+        double avg = total / 3.0;
 
         displayStudent();
-
-        System.out.println("Marks 1 : " + marks1);
-        System.out.println("Marks 2 : " + marks2);
-        System.out.println("Marks 3 : " + marks3);
-
-        int total = marks1 + marks2 + marks3;
-        double average = total / 3.0;
-
-        System.out.println("Total   : " + total);
-        System.out.println("Average : " + average);
+        System.out.println("Marks1: " + marks1);
+        System.out.println("Marks2: " + marks2);
+        System.out.println("Marks3: " + marks3);
+        System.out.println("Total: " + total);
+        System.out.println("Average: " + avg);
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Roll No: ");
-        int rollNo = sc.nextInt();
-
+        System.out.print("Enter roll no: ");
+        int r = sc.nextInt();
         sc.nextLine();
 
-        System.out.print("Enter Name: ");
-        String name = sc.nextLine();
+        System.out.print("Enter name: ");
+        String n = sc.nextLine();
 
-        System.out.print("Enter Marks 1: ");
+        System.out.print("Enter marks1: ");
         int m1 = sc.nextInt();
-
-        System.out.print("Enter Marks 2: ");
+        System.out.print("Enter marks2: ");
         int m2 = sc.nextInt();
-
-        System.out.print("Enter Marks 3: ");
+        System.out.print("Enter marks3: ");
         int m3 = sc.nextInt();
 
-        Result r = new Result(rollNo, name, m1, m2, m3);
-
-        System.out.println("\n--- MARK SHEET ---");
-        r.displayResult();
+        Result res = new Result(r, n, m1, m2, m3);
+        System.out.println("\n--- Marksheet ---");
+        res.displayResult();
     }
 }
